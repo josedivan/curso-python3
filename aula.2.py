b3b1c822f1c0df4b9fd522018afdfc694c748a01
Jose Edivan Lima <edivanlima23@hotmail.com>
2022-12-28 22:28:15 -0300

    first commit

---
+++ aula.2.py
@@ -0,0 +1,24 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('jose')
+print(1, 'Luiz "Otávio"')
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# Escape
+print("jose \"lima\"")
+
+# r
+print(r"Luiz \"Otávio\"")
+
+Print('Python')
